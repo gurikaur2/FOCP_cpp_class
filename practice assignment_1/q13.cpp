03dcd638b48c4b6de81efa8a9042710f8078adc2
gurikaur2 <gurnoorkaur.mail@gmail.com>
2025-01-29 09:44:15 +0530

    Initial commit

--- practice assignment_1/q13.cpp
+++ practice assignment_1/q13.cpp
@@ -6,9 +6,34 @@ using namespace std;
 
 int main ()
 {
-    int i,choice;
-    cout<<"Enter a choice:\n 1. Withdraw amount\n 2. Deposit amount\n 3. Balance Inquiry\n";
-    
+    int i,choice,amount, balance=100000;
+    cout<<"Enter a choice:\n 1. Withdraw amount\n 2. Deposit amount\n 3. Balance Inquiry\n 4. Exit\n";
+    cin>>choice;
+    switch (choice)
+    {
+    case 1:
+    cout<<"Enter amount to be withdrawn:";
+    cin>>amount;
+    break;
+    case 2:
+    cout<<"Enter amount to be deposited:";
+    cin>>amount;
+    if (amount>balance)
+    {
+        cout<<"Insufficient balance!";
+    }
+    break;
+    case 3:
+    cout<<"Balance ="<<balance;
+    break;
+    case 4:
+    cout<<"EXIT";
+    break;
+    default:
+    cout<<"Invalid choice!";
+    break;
+    }
+
 
     return 0;
 }
